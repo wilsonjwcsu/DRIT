5d1b0605140399a7fc48b3e9f64cfefd80b04c78
Jesse Wilson <jesse.wilson@colostate.edu>
2022-01-04 09:42:27 -0700

    changed HistoCAE normalization to InstanceNorm

--- src/networks.py
+++ src/networks.py
@@ -95,28 +95,28 @@ def get_enc_histoCAE(nc_in):
     net = [
           nn.Conv2d(nc_in,16,kernel_size=3,stride=1,padding=1), # layer 1, 256x256x16
           nn.ELU(),
-          nn.BatchNorm2d(16),                                   # layer 2, 256x256x16
+          nn.InstanceNorm2d(16,affine=True),                                   # layer 2, 256x256x16
           nn.Conv2d(16,16,kernel_size=3,stride=2,padding=1),    # layer 3, 128x128x16
           nn.ELU(),
-          nn.BatchNorm2d(16),                                   # layer 4, 128x128x16
+          nn.InstanceNorm2d(16,affine=True),                                   # layer 4, 128x128x16
           nn.Conv2d(16,32,kernel_size=3,stride=1,padding=1),    # layer 5, 128x128x32
           nn.ELU(),
-          nn.BatchNorm2d(32),                                   # layer 6, 128x128x32
+          nn.InstanceNorm2d(32,affine=True),                                   # layer 6, 128x128x32
           nn.Conv2d(32,32,kernel_size=3,stride=2,padding=1),    # layer 7, 64x64x32
           nn.ELU(),
-          nn.BatchNorm2d(32),                                   # layer 8, 64x64x32
+          nn.InstanceNorm2d(32,affine=True),                                   # layer 8, 64x64x32
           nn.Conv2d(32,64,kernel_size=3,stride=1,padding=1),    # layer 9, 64x64x64
           nn.ELU(),
-          nn.BatchNorm2d(64),                                   # layer 10, 64x64x64
+          nn.InstanceNorm2d(64,affine=True),                                   # layer 10, 64x64x64
           nn.Conv2d(64,64,kernel_size=3,stride=2,padding=1),    # layer 11, 32x32x64
           nn.ELU(),
-          nn.BatchNorm2d(64),                                   # layer 12, 32x32x64
+          nn.InstanceNorm2d(64,affine=True),                                   # layer 12, 32x32x64
           nn.Conv2d(64,64,kernel_size=3,stride=1,padding=1),    # layer 13, 32x32x64
           nn.ELU(),
-          nn.BatchNorm2d(64),                                   # layer 14, 32x32x64
+          nn.InstanceNorm2d(64,affine=True),                                   # layer 14, 32x32x64
           nn.Conv2d(64,64,kernel_size=3,stride=2,padding=1),    # layer 15, 16x16x64
-          nn.ELU(),
-          nn.BatchNorm2d(64)                                    # layer 16, 16x16x64
+          nn.Tanh(),
+          nn.InstanceNorm2d(64,affine=True)                                    # layer 16, 16x16x64
           ]
 
     return nn.Sequential(*net)
@@ -125,28 +125,28 @@ def get_enc_histoCAE_wide(nc_in, w):
     net = [
           nn.Conv2d(nc_in,w,kernel_size=3,stride=1,padding=1), # layer 1, 256x256xw
           nn.ELU(),
-          nn.BatchNorm2d(w),                                   # layer 2, 256x256xw
+          nn.InstanceNorm2d(w,affine=True),                                   # layer 2, 256x256xw
           nn.Conv2d(w,w,kernel_size=3,stride=2,padding=1),     # layer 3, 128x128xw
           nn.ELU(),
-          nn.BatchNorm2d(w),                                   # layer 4, 128x128xw
+          nn.InstanceNorm2d(w,affine=True),                                   # layer 4, 128x128xw
           nn.Conv2d(w,w,kernel_size=3,stride=1,padding=1),     # layer 5, 128x128xw
           nn.ELU(),
-          nn.BatchNorm2d(w),                                   # layer 6, 128x128xw
+          nn.InstanceNorm2d(w,affine=True),                                   # layer 6, 128x128xw
           nn.Conv2d(w,w,kernel_size=3,stride=2,padding=1),     # layer 7, 64x64xw
           nn.ELU(),
-          nn.BatchNorm2d(w),                                   # layer 8, 64x64xw
+          nn.InstanceNorm2d(w,affine=True),                                   # layer 8, 64x64xw
           nn.Conv2d(w,w,kernel_size=3,stride=1,padding=1),     # layer 9, 64x64xw
           nn.ELU(),
-          nn.BatchNorm2d(w),                                   # layer 10, 64x64xw
+          nn.InstanceNorm2d(w,affine=True),                                   # layer 10, 64x64xw
           nn.Conv2d(w,w,kernel_size=3,stride=2,padding=1),     # layer 11, 32x32xw
           nn.ELU(),
-          nn.BatchNorm2d(w),                                   # layer 12, 32x32xw
+          nn.InstanceNorm2d(w,affine=True),                                   # layer 12, 32x32xw
           nn.Conv2d(w,w,kernel_size=3,stride=1,padding=1),     # layer 13, 32x32xw
           nn.ELU(),
-          nn.BatchNorm2d(w),                                   # layer 14, 32x32xw
+          nn.InstanceNorm2d(w,affine=True),                                   # layer 14, 32x32xw
           nn.Conv2d(w,64,kernel_size=3,stride=2,padding=1),    # layer 15, 16x16x64
-          nn.ELU(),
-          nn.BatchNorm2d(64)                                   # layer 16, 16x16x64
+          nn.Tanh(),
+          nn.InstanceNorm2d(64)                                   # layer 16, 16x16x64
           ]
 
     return nn.Sequential(*net)
@@ -365,31 +365,31 @@ def get_dec_histoCAE(nc_out):
     net = [
         nn.Conv2d(64,64,kernel_size=3,padding=1),           # layer 17, 16x16x64
         nn.ELU(),
-        nn.BatchNorm2d(64),                                 # layer 18, 16x16x64
+        nn.InstanceNorm2d(64,affine=True),                                 # layer 18, 16x16x64
         nn.Conv2d(64,64,kernel_size=3,padding=1),           # layer 19, 16x16x64
         nn.ELU(),
-        nn.BatchNorm2d(64),                                 # layer 20, 16x16x64
+        nn.InstanceNorm2d(64,affine=True),                                 # layer 20, 16x16x64
         nn.Upsample(scale_factor=(2,2),mode='bilinear'),    # layer 21, 32x32x64
         nn.Conv2d(64,64,kernel_size=3,padding=1),           # layer 22, 32x32x64
         nn.ELU(),
-        nn.BatchNorm2d(64),                                 # layer 23, 32x32x64
+        nn.InstanceNorm2d(64,affine=True),                                 # layer 23, 32x32x64
         nn.Conv2d(64,64,kernel_size=3,padding=1),           # layer 24, 32x32x64
         nn.ELU(),
-        nn.BatchNorm2d(64),                                 # layer 25, 32x32x64
+        nn.InstanceNorm2d(64,affine=True),                                 # layer 25, 32x32x64
         nn.Upsample(scale_factor=(2,2),mode='bilinear'),    # layer 26, 64x64x64
         nn.Conv2d(64,32,kernel_size=3,padding=1),           # layer 27, 64x64x32
         nn.ELU(),
-        nn.BatchNorm2d(32),                                 # layer 28, 64x64x32
+        nn.InstanceNorm2d(32,affine=True),                                 # layer 28, 64x64x32
         nn.Conv2d(32,32,kernel_size=3,padding=1),           # layer 29, 64x64x32
         nn.ELU(),
-        nn.BatchNorm2d(32),                                 # layer 30, 64x64x32
+        nn.InstanceNorm2d(32,affine=True),                                 # layer 30, 64x64x32
         nn.Upsample(scale_factor=(2,2),mode='bilinear'),    # layer 31, 128x128x32
         nn.Conv2d(32,16,kernel_size=3,padding=1),           # layer 32, 128x128x16
         nn.ELU(),
-        nn.BatchNorm2d(16),                                 # layer 33, 128x128x16
+        nn.InstanceNorm2d(16,affine=True),                                 # layer 33, 128x128x16
         nn.Conv2d(16,16,kernel_size=3,padding=1),           # layer 34, 128x128x16
         nn.ELU(),
-        nn.BatchNorm2d(16),                                 # layer 35, 128x128x16
+        nn.InstanceNorm2d(16,affine=True),                                 # layer 35, 128x128x16
         nn.Upsample(scale_factor=(2,2),mode='bilinear'),    # layer 36, 256x256x16
         nn.Conv2d(16,nc_out,kernel_size=3,padding=1),       # layer 37, 256x256x3
         nn.Tanh()                                           # HistoCAE used Sigmoid, we're using Tanh
